ce3c0f59a7600a57eb6393789958818d6b05993e
Fanelesibonge Hlebani <112683822+Sov3r3ign@users.noreply.github.com>
2026-09-12 20:17:09 +0200

    chore: remove Lovable scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
     tailwindcss(),
     tanstackStart(),
     react(),
   ],
+
+  resolve: {
+    tsconfigPaths: true,
+  },
 
   server: {
     port: 8080,
